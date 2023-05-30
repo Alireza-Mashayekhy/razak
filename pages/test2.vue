b8d2ce460e16a712c6a7d6df10ce5d36fe022a1d
alireza <alirezamashayekhy2000@gmail.com>
2023-05-30 13:53:44 +0330

    change loading

--- pages/test2.vue
+++ pages/test2.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="loading">
-    <img src="/images/enLogo.svg " class="logo" alt="logo" />
+    <!-- <div class="effect">
+      <img src="/images/enLogo.svg " class="logo" alt="logo" />
+      <span class="dot1 dot"></span>
+      <span class="dot2 dot"></span>
+      <span class="dot3 dot"></span>
+      <span class="dot4 dot"></span>
+      <span class="dot5 dot"></span>
+      <span class="dot6 dot"></span>
+    </div> -->
+    <svg viewBox="0 0 960 300">
+      <symbol id="s-text">
+        <text text-anchor="middle" x="50%" y="80%">Razak</text>
+      </symbol>
+      <g class="g-ants">
+        <use xlink:href="#s-text" class="text-copy"></use>
+        <use xlink:href="#s-text" class="text-copy"></use>
+        <use xlink:href="#s-text" class="text-copy"></use>
+        <use xlink:href="#s-text" class="text-copy"></use>
+        <use xlink:href="#s-text" class="text-copy"></use>
+      </g>
+    </svg>
   </div>
 </template>
 
