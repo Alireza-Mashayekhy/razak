a4328a502163cebfd65a58b70fa03353bdda3a50
alireza <alirezamashayekhy2000@gmail.com>
2023-06-12 17:35:19 +0330

    some changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="loading">
-    <svg viewBox="0 0 960 300">
-      <symbol id="s-text">
-        <text text-anchor="middle" x="50%" y="80%">Razak</text>
-      </symbol>
-      <g class="g-ants">
-        <use xlink:href="#s-text" class="text-copy"></use>
-        <use xlink:href="#s-text" class="text-copy"></use>
-        <use xlink:href="#s-text" class="text-copy"></use>
-        <use xlink:href="#s-text" class="text-copy"></use>
-        <use xlink:href="#s-text" class="text-copy"></use>
-      </g>
-    </svg>
+    <div class="logo">
+      <div class="text">Razak</div>
+    </div>
   </div>
 </template>
 
